4a0de161f022a211abe84d0a535049a1778a0a6f
Jessica Chong <jxchong@gmail.com>
2013-02-21 22:45:48 -0800

    improved speed of adding and fixed bug

--- add_error_populationfreqs.pl
+++ add_error_populationfreqs.pl
@@ -44,6 +44,7 @@ if ($inputfile !~ m/SSAnnotation/) {
 	die "Input file ($inputfile) isn't an SSAnnotation file\n";
 }
 
+my %chr_contents = ();
 my $workingchr = "NA";
 open (FILE, "$inputfile") or die "Cannot read $inputfile file: $!.\n";
 my $headerline = <FILE>;
@@ -54,21 +55,48 @@ while ( <FILE> ) {
 	$_ =~ s/\s+$//;					# Remove line endings
 	my @line = split ("\t", $_);
 	my ($chr, $pos, $vartype, $ref, $alt) = @line[0..4];
+
+	# if ($pos < 207015957) {
+	# 	next;
+	# }
+	# if ($pos > 207304900) {
+	# 	exit;
+	# }
 	
-	my %chr_contents;
 	if ($chr ne $workingchr) {
 		my $maxmafs_ref = readData($chr);
 		%chr_contents = %{$maxmafs_ref};
-		$workingchr = $chr;		
+		$workingchr = $chr;
+		# my $fakelookup = "207304900.T.C";
+		# print "static 207304900.T.C: $chr_contents{'207304900.T.C'}{'pop'}\n";
+		# print "fakelookup = $fakelookup : $chr_contents{$fakelookup}{'pop'}\n";
+		# print "\n";
 	}
+	
+	# print "\npos=$pos, ref=$ref, alt=$alt\n";
+	# while (my ($lookup, $val) = each %chr_contents) {
+	# 	print "$lookup -> $val ($chr_contents{$lookup}{'pop'})\n";
+	# }
+	# my $fakelookup = "207304900.T.C";
+	# print "1 static 207304900.T.C: $chr_contents{'207304900.T.C'}{'pop'}\n";
+	# print "1 fakelookup = $fakelookup : $chr_contents{$fakelookup}{'pop'}\n";
+	# print "\n";
 	
 	my ($errorfreq, $maxpopmaf) = (0, 0);
 	my $lookup = "$pos.$ref.$alt";
-	if (defined $chr_contents{$lookup}) {
+	if (exists $chr_contents{$lookup}{'pop'}) {
 		$errorfreq = $chr_contents{$lookup}{'error'};
 		$maxpopmaf = $chr_contents{$lookup}{'pop'};
-	}
-
+	}	
+	# print "2 lookup = $lookup : $chr_contents{$lookup}{'pop'}\n";
+	# print "2 fakelookup = $fakelookup : $chr_contents{$fakelookup}{'pop'}\n";
+	# if (defined $chr_contents{$lookup}{'pop'}) {
+	# 	print "lookup exists\n";
+	# }
+	# if (defined $chr_contents{$fakelookup}{'pop'}) {
+	# 	print "fake lookup exists\n";
+	# }
+	
 	print OUT join("\t", @line)."\t".sprintf("%.4f", $errorfreq)."\t".sprintf("%.4f", $maxpopmaf)."\n";
 }
 close FILE;
@@ -95,134 +123,20 @@ sub readData {
 		die "Error: exit value $exit_value\n@popdata\n";
 	}
 	foreach (@popdata) {
+		$_ =~ s/\s+$//;					# Remove line endings
+		# print "storing $_\n";
 		my ($chr, $varstart, $varend, $ref, $alt, $freqinCMG, $freqinOutside) = split("\t", $_);
 		my $lookup = "$varstart.$ref.$alt";
 		$maxmafs{$lookup}{'error'} = $freqinCMG;
 		$maxmafs{$lookup}{'pop'} = $freqinOutside;
+		# print "stored $maxmafs{$lookup}{'pop'} in $lookup\n";
 	}
-
 	return \%maxmafs;
 }
 
 
 
 
-
-
-
-
-
-
-# old code, very slow; reads from multiple files
-# 
-# sub isCommonVar {
-# 	# remove common variation (ESP and 1kG)
-# 	my ($targetchr, $targetpos, $targettype, $targetref, $targetalt, $mafcutoff) = @_;
-# 	
-# 	my $commonvarpath = '/net/grc/vol1/mendelian_projects/mendelian_analysis/references';
-# 	my $thousandgenomesfile = "$commonvarpath/phase1_release_v3.20101123.snps_indels_svs.sites.vcf.gz";
-# 	my $espSNPsfile = "$commonvarpath/ESP6500.snps.vcf.gz";
-# 	my $espindelsfile = "$commonvarpath/esp6500_indels.frq";
-# 	my $africanSNPsfile = "$commonvarpath/Gibbons_1.frq.gz";
-# 		
-# 	my $maxmaf = 0;
-# 	if ($targettype =~ m/snp/i) {
-# 		## to do: implement indel check against ESP when Josh releases the data
-# 		my @varatsamepos = `tabix $espSNPsfile $targetchr:$targetpos-$targetpos`;
-# 		foreach my $variant (@varatsamepos) {
-# 			my ($varchr, $varpos, $rsid, $varref, $varalt, @vardata) = split("\t", $variant);
-# 			$vardata[2] =~ /MAF=((\.|\d|,)+);/;
-# 			my @popMAFs = split(",", $1);
-# 			if ($varpos == $targetpos && $varref eq $targetref && $varalt eq $targetalt) {
-# 				foreach my $varmaf (@popMAFs) {
-# 					my $actualmaf = $varmaf/100;						# in ESP, allele freqs reported as percentages
-# 					if ($actualmaf > $maxmaf) {
-# 						$maxmaf = $actualmaf;
-# 					}
-# 				}
-# 			}
-# 		}
-# 		
-# 		@varatsamepos = `tabix $africanSNPsfile $targetchr:$targetpos-$targetpos`;
-# 		foreach my $variant (@varatsamepos) {
-# 			my ($varchr, $varpos, $nalleles, $nchrobs, $reffreq, $altfreq) = split("\t", $variant);
-# 			# if ($varpos == $targetpos && $varref eq $targetref && $varalt eq $targetalt) {
-# 			# 	foreach my $varmaf (@popMAFs) {
-# 					if ($altfreq > $maxmaf) {
-# 						$maxmaf = $altfreq;
-# 					}
-# 				# }
-# 			# }
-# 		}
-# 	}
-# 	
-# 	my @varatsamepos = `tabix $thousandgenomesfile $targetchr:$targetpos-$targetpos`;
-# 	foreach my $variant (@varatsamepos) {
-# 		my ($varchr, $varpos, $rsid, $varref, $varalt, @vardata) = split("\t", $variant);
-# 		my @populations = qw(ASN AMR AFR EUR);
-# 		my @popMAFs;
-# 		foreach my $population (@populations) {
-# 			my $maffield = $population."_AF";
-# 			$vardata[2] =~ m/;$maffield=((\.|\d)+)/;
-# 			if (defined $1) {
-# 				push(@popMAFs, $1);
-# 			}
-# 		}
-# 		if ($varpos == $targetpos && $varref eq $targetref && $varalt eq $targetalt) {
-# 			foreach my $varmaf (@popMAFs) {
-# 				if ($varmaf > $maxmaf) {
-# 					$maxmaf = $varmaf;
-# 				}
-# 			}
-# 		}
-# 	}
-# 	
-# 	return $maxmaf;
-# }
-# 
-# sub isSystematicError {
-# 	# remove systematic errors
-# 	my ($targetchr, $targetpos, $targettype, $targetref, $targetalt, $capturearray) = @_;
-# 		
-# 	my $errorpath = '/net/grc/vol1/mendelian_projects/mendelian_analysis/references/systematic_error/2012_oct';
-# 	my ($snperrorsfile, $indelerrorsfile);
-# 	if ($capturearray eq 'bigexome' || $capturearray eq 'v3') {
-# 		$snperrorsfile = "$errorpath/snv.bigexome.vcf.gz";	
-# 		$indelerrorsfile = "$errorpath/indels.bigexome.vcf.gz";	
-# 	} elsif ($capturearray eq 'v2') {
-# 		$snperrorsfile = "$errorpath/snv.v2.vcf.gz";	
-# 		$indelerrorsfile = "$errorpath/indels.v2.vcf.gz";
-# 	} else {
-# 		$snperrorsfile = "$errorpath/snv.$capturearray.vcf.gz";	
-# 		$indelerrorsfile = "$errorpath/indels.$capturearray.vcf.gz";
-# 	}
-# 
-# 	my @errors;
-# 	if ($targettype =~ m/snp/i) {
-# 		@errors = `tabix $snperrorsfile $targetchr:$targetpos-$targetpos`;
-# 	} elsif ($targettype =~ m/indel/i) {
-# 		@errors = `tabix $indelerrorsfile $targetchr:$targetpos-$targetpos`;
-# 	}
-# 	
-# 	my $maxerrorfreq = 0;
-# 	foreach my $error (@errors) {
-# 		my ($errorchr, $errorpos, $errortype, $errorref, $erroralt, @errordata) = split("\t", $error);
-# 		my @errorfreqinfo = split("/", $errordata[2]);
-# 		$errorfreqinfo[0] =~ s/OBSERVED=//;
-# 		my $errorfreq = 0;
-# 		if ($errorfreqinfo[1] != 0) {
-# 			$errorfreq = $errorfreqinfo[0]/$errorfreqinfo[1];
-# 		}
-# 		if ($errorpos == $targetpos && $errorref eq $targetref && $erroralt eq $targetalt) {
-# 			if ($errorfreq >= $maxerrorfreq) {
-# 				$maxerrorfreq = $errorfreq;
-# 			}
-# 		}
-# 	}
-# 	
-# 	return $maxerrorfreq;
-# }
-# 
 
 
 
